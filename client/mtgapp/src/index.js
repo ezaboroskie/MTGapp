c8de5029333acbd6d1fc191afa3a36e51c46f927
Eddie Zaboroskie <Zaboroskie.Edward@gmail.com>
2022-12-13 17:17:45 -0500

    fixedauthissue

--- client/mtgapp/src/index.js
+++ client/mtgapp/src/index.js
@@ -21,6 +21,11 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
+const token = localStorage.getItem('jwt')
+if(token){
+  store.dispatch({type:'ON_LOGIN', payload:token})
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
